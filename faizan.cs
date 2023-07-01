76df2f44a8b3ad3016112374cda4020f16134970
faizansavanti <107442515+faizansavanti@users.noreply.github.com>
2023-07-01 13:25:45 -0600

    Update faizan.cs by faizansavanti

--- faizan.cs
+++ faizan.cs
@@ -2,6 +2,7 @@ class Program
 {
     public void Main(string[] args)
     {
-        System.Console.WriteLine("This is faizan's class");
+        System.Console.WriteLine("This is added by faizan");
+        System.Console.WriteLine("This is added by faizansavanti");
     }
 }
